5806cc62115c641f1a7d82962d4f491794c31022
jblanked <82678820+jblanked@users.noreply.github.com>
2024-12-03 10:54:57 -0500

    Restructure

--- ESP32-WROVER/flipper-http.ino
+++ ESP32-WROVER/flipper-http.ino
@@ -2,8 +2,9 @@
 Author: JBlanked
 Github: https://github.com/jblanked/FlipperHTTP
 Info: This library is a wrapper around the HTTPClient library and is used to communicate with the FlipperZero over serial.
+Board Manager: ESP32 Wrover Module
 Created: 2024-09-30
-Updated: 2024-11-09
+Updated: 2024-12-03
 */
 
 #include <FlipperHTTP.h>
