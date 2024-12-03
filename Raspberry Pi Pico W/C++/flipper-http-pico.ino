95d10926dd7e197e1f8221ee6fe4c97fb20bc626
jblanked <82678820+jblanked@users.noreply.github.com>
2024-12-02 20:41:02 -0500

    Restructure

--- Raspberry Pi Pico W/C++/flipper-http-pico.ino
+++ Raspberry Pi Pico W/C++/flipper-http-pico.ino
@@ -2,8 +2,9 @@
 Author: JBlanked
 Github: https://github.com/jblanked/FlipperHTTP
 Info: This library is a wrapper around the HTTPClient library and is used to communicate with the FlipperZero over serial.
+Board Manager: Raspberry Pi Pico W
 Created: 2024-10-24
-Updated: 2024-11-15
+Updated: 2024-12-02
 */
 
 #include "FlipperHTTPPico.h"
